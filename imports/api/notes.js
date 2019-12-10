e7ef8228b16ac6befb610676c01743b6c2fe62a9
William L <w-lindvall@outlook.com>
2019-12-09 20:13:24 -0800

    refactor new note call to meteor method

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -1,3 +1,22 @@
+import {Meteor} from 'meteor/meteor'
 import {Mongo} from 'meteor/mongo'
+import {check} from 'meteor/check'
 
-export default new Mongo.Collection('notes')
+const Notes = new Mongo.Collection('notes')
+
+Meteor.methods({
+  'notes.insert'(title) {
+    check(title, String)
+
+    if (!this.userId) throw new Meteor.Error('not-authorized')
+
+    Notes.insert({
+      title,
+      createdAt: new Date(),
+      author: this.userId,
+      // username: Meteor.users.findOne(this.userId).username,
+    })
+  },
+})
+
+export default Notes
