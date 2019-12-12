a7df46dd0de81e070fc6e44634d2354384047f03
William L <w-lindvall@outlook.com>
2019-12-11 21:11:57 -0800

    init custom auth form

--- imports/ui/components/AuthForm.jsx
+++ imports/ui/components/AuthForm.jsx
@@ -1,25 +1,85 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
-import {Template} from 'meteor/templating'
-import {Blaze} from 'meteor/blaze'
+import React, {useState} from 'react'
+import {Meteor} from 'meteor/meteor'
+import {Accounts} from 'meteor/accounts-base'
+import {useFormik} from 'formik'
 
-class AuthForm extends Component {
-  componentDidMount() {
-    this.view = Blaze.render(
-      Template.loginButtons,
-      // eslint-disable-next-line
-      ReactDOM.findDOMNode(this.refs.container),
-    )
-  }
+const onAuth = e => {
+  if (e) alert(e)
+  alert('logged in')
+  // TODO: clear form
+}
 
-  componentWillUnmount() {
-    Blaze.remove(this.view)
-  }
+const Field = ({id, label, name, type, ...props}) => (
+  <>
+    <label htmlFor={name}>{label}</label>
+    <input id={id} name={name} type={type} {...props} />
+  </>
+)
 
-  render() {
-    // eslint-disable-next-line
-    return <span ref="container" />
-  }
+const AuthForm = props => {
+  const [isSignUp, setIsSignUp] = useState(false)
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+    onSubmit: ({username, email, password}) => {
+      isSignUp
+        ? Accounts.createUser({username, email, password}, onAuth)
+        : Meteor.loginWithPassword(email, password, onAuth)
+    },
+    // TODO: validate
+  })
+  return (
+    <form onSubmit={formik.handleSubmit} {...props}>
+      {isSignUp ? (
+        <Field
+          label="Username"
+          id="username"
+          name="username"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.username}
+        />
+      ) : null}
+
+      <Field
+        label="Email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <Field
+        label="Password"
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
+
+      {isSignUp ? (
+        <Field
+          label="Confirm password"
+          id="passwordConfirm"
+          name="passwordConfirm"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.passwordConfirm}
+        />
+      ) : null}
+
+      <button type="submit">Submit</button>
+
+      <button onClick={() => setIsSignUp(s => !s)} type="button">
+        {isSignUp ? 'Already have an account' : 'Create an account'}
+      </button>
+    </form>
+  )
 }
 
 export default AuthForm
