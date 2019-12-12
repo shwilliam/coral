ee11aa108b21d1c76be6c5bba898a2690271b781
EdinK1 <edinkaymakchi@gmail.com>
2019-12-11 20:50:45 -0800

    feat: setup email in withUser hoc

--- imports/ui/hocs/withUser.js
+++ imports/ui/hocs/withUser.js
@@ -1,8 +1,13 @@
-import {Meteor} from 'meteor/meteor'
-import {withTracker} from 'meteor/react-meteor-data'
+import { Meteor } from 'meteor/meteor'
+import { withTracker } from 'meteor/react-meteor-data'
 
-const withUser = withTracker(() => ({
-  user: Meteor.user(),
-}))
+const withUser = withTracker(() => {
+  const user = Meteor.user()
+
+  return {
+    user,
+    email: user && user.emails[0] && user.emails[0].address
+  }
+})
 
 export default withUser
--- imports/ui/pages/Profile.jsx
+++ imports/ui/pages/Profile.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { Descriptions } from 'antd'
 import withUser from '../hocs/withUser'
 
-const Profile = ({ user }) =>
+const Profile = ({ user, email }) =>
   user ? (
     <Descriptions title='User Info'>
       <Descriptions.Item label='UserName'>[username]</Descriptions.Item>
-      <Descriptions.Item label='Email'>
-        {user.emails[0] && user.emails[0].address}
-      </Descriptions.Item>
+      <Descriptions.Item label='Email'>{email}</Descriptions.Item>
     </Descriptions>
   ) : (
     <p>loading</p>
