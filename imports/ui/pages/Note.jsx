eb99fbb69b5ba84a224d5ce3bc6716a72ed53f62
Roberto H <robhhr@outlook.com>
2019-12-12 21:46:20 -0800

    feat: add rich text note template

--- imports/ui/pages/Note.jsx
+++ imports/ui/pages/Note.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useParams } from 'react-router'
 import withUser from '../hocs/withUser'
+import NoteTemplate from '../components/NoteTemplate'
+
 const Note = ({ user }) => {
   let { username, id } = useParams()
   return (
     <h1>
-      hello {username} | {id}
+      {/* hello {username} | {id} */}
+      <NoteTemplate />
+
     </h1>
   )
 }
