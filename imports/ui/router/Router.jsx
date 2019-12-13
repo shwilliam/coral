971b0bff130003da55de5ef3d02d6f341dc744e8
William L <w-lindvall@outlook.com>
2019-12-13 04:07:53 -0800

    set up temp notes page

--- imports/ui/router/Router.jsx
+++ imports/ui/router/Router.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Router, Route, Switch} from 'react-router'
 const createBrowserHistory = require('history').createBrowserHistory
+
 import Home from '../pages/Home'
 import FourOhFour from '../pages/FourOhFour'
 import Note from '../pages/Note'
+import Notes from '../pages/Notes'
 import Profile from '../pages/Profile'
 
 const browserHistory = createBrowserHistory()
@@ -11,6 +13,8 @@ const browserHistory = createBrowserHistory()
 const Routes = () => (
   <Router history={browserHistory}>
     <Switch>
+      {/* TODO: delete notes page */}
+      <Route exact path="/notes" component={Notes} />
       <Route exact path="/" component={Home} />
       <Route exact path="/:username/:id" component={Note} />
       <Route exact path="/profile" component={Profile} />
