61555c5bd4851384eb52e7c8f7d7737eb83aefb3
EdinK1 <edinkaymakchi@gmail.com>
2019-12-12 23:44:48 -0800

    feat: add footer to layout

--- imports/ui/pages/Profile.jsx
+++ imports/ui/pages/Profile.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Descriptions } from 'antd'
 import withUser from '../hocs/withUser'
+import Copyright from '../components/Copyright'
 
 const Profile = ({ user, email }) =>
   user ? (
-    <Descriptions title='User Info'>
-      <Descriptions.Item label='UserName'>[username]</Descriptions.Item>
-      <Descriptions.Item label='Email'>{email}</Descriptions.Item>
-    </Descriptions>
+    <>
+      <Descriptions title='User Info'>
+        <Descriptions.Item label='UserName'>[username]</Descriptions.Item>
+        <Descriptions.Item label='Email'>{email}</Descriptions.Item>
+      </Descriptions>
+      <Copyright />
+    </>
   ) : (
-    <p>loading</p>
+    <p>loading...</p>
   )
 
 export default withUser(Profile)
