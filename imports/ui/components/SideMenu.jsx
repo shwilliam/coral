957b8315be1fe9834763055ef019d5f577a918db
William L <w-lindvall@outlook.com>
2019-12-12 20:39:20 -0800

    move antd stylesheet import to `main.jsx`

--- imports/ui/components/SideMenu.jsx
+++ imports/ui/components/SideMenu.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 // import Notes from './Notes'
-import 'antd/dist/antd.css' // TODO: use modularized styles
-import { Menu, Icon, Layout } from 'antd'
+import {Menu, Icon, Layout} from 'antd'
 
-const { Sider } = Layout
+const {Sider} = Layout
 
 const SideMenu = () => {
-
   const [collapsed, setCollapsed] = useState(false)
 
   return (
@@ -24,7 +22,7 @@ const SideMenu = () => {
         }}
       >
         <div className="logo" />
-        <Menu theme='dark' mode="inline" defaultSelectedKeys={['4']}>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
           <Menu.Item key="1">
             <Icon type="user" />
             <span className="nav-text">Notes</span>
