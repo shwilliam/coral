459c0ec2d412aae6fda3e832730eaa2f673f9c76
EdinK1 <edinkaymakchi@gmail.com>
2019-12-15 02:51:43 -0800

    feat: use different navbar for mobile

--- imports/ui/components/Layout.jsx
+++ imports/ui/components/Layout.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import {Layout as AntLayout} from 'antd'
 import SideMenu from '../components/SideMenu'
 import Footer from '../components/Footer'
+import MobileMenu from './NoteEditor/MobileMenu'
 const {Content} = AntLayout
+
+const mobile = '414px'
 
 const Layout = ({children, ...props}) => (
   <AntLayout>
-    <SideMenu />
+    {/* <SideMenu /> */}
+    <MobileMenu />
     <AntLayout>
       <Content>{children}</Content>
       <Footer />
--- imports/ui/components/NoteEditor/MobileMenu.jsx
+++ imports/ui/components/NoteEditor/MobileMenu.jsx
@@ -1,23 +1,63 @@
 import React from 'react'
-import {Tabs} from 'antd'
+const {SubMenu} = Menu
+import {Menu, Icon} from 'antd'
 
-const {TabPane} = Tabs
-
-function callback(key) {
-  console.log(key)
-}
-
-ReactDOM.render(
-  <Tabs defaultActiveKey="1" onChange={callback}>
-    <TabPane tab="Tab 1" key="1">
-      Content of Tab Pane 1
-    </TabPane>
-    <TabPane tab="Tab 2" key="2">
-      Content of Tab Pane 2
-    </TabPane>
-    <TabPane tab="Tab 3" key="3">
-      Content of Tab Pane 3
-    </TabPane>
-  </Tabs>,
-  mountNode,
+const MobileMenu = ({notes, sharedNotes, ...props}) => (
+  <Menu
+    style={{textAlign: 'center'}}
+    onClick={() => console.log('hey')}
+    // selectedKeys={}
+    mode="horizontal"
+  >
+    <SubMenu
+      title={
+        <span className="submenu-title-wrapper">
+          <Icon type="edit" />
+        </span>
+      }
+    >
+      <Menu.ItemGroup title="Item 1">
+        <Menu.Item key="setting:1">Option 1</Menu.Item>
+        <Menu.Item key="setting:2">Option 2</Menu.Item>
+      </Menu.ItemGroup>
+      <Menu.ItemGroup title="Item 2">
+        <Menu.Item key="setting:3">Option 3</Menu.Item>
+        <Menu.Item key="setting:4">Option 4</Menu.Item>
+      </Menu.ItemGroup>
+    </SubMenu>
+    <SubMenu
+      title={
+        <span className="submenu-title-wrapper">
+          <Icon type="team" />
+        </span>
+      }
+    >
+      <Menu.ItemGroup title="Item 1">
+        <Menu.Item key="setting:1">Option 1</Menu.Item>
+        <Menu.Item key="setting:2">Option 2</Menu.Item>
+      </Menu.ItemGroup>
+      <Menu.ItemGroup title="Item 2">
+        <Menu.Item key="setting:3">Option 3</Menu.Item>
+        <Menu.Item key="setting:4">Option 4</Menu.Item>
+      </Menu.ItemGroup>
+    </SubMenu>
+    <SubMenu
+      title={
+        <span className="submenu-title-wrapper">
+          <Icon type="setting" />
+        </span>
+      }
+    >
+      <Menu.ItemGroup title="Item 1">
+        <Menu.Item key="setting:1">Option 1</Menu.Item>
+        <Menu.Item key="setting:2">Option 2</Menu.Item>
+      </Menu.ItemGroup>
+      <Menu.ItemGroup title="Item 2">
+        <Menu.Item key="setting:3">Option 3</Menu.Item>
+        <Menu.Item key="setting:4">Option 4</Menu.Item>
+      </Menu.ItemGroup>
+    </SubMenu>
+  </Menu>
 )
+
+export default MobileMenu
