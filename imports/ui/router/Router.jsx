76bb6f4eed244a6a8b1863f043499648272987fb
Roberto H <robhhr@outlook.com>
2019-12-16 01:13:00 -0800

    disable welcome page for logged in users

--- imports/ui/router/Router.jsx
+++ imports/ui/router/Router.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Router, Route, Switch, Redirect} from 'react-router'
 const createBrowserHistory = require('history').createBrowserHistory
-
+import {useHistory} from 'react-router'
 import Home from '../pages/Home'
 import FourOhFour from '../pages/FourOhFour'
 import Note from '../pages/Note'
@@ -9,19 +9,37 @@ import Notes from '../pages/Notes'
 import Profile from '../pages/Profile'
 import Welcome from '../pages/Welcome'
 import ProtectedRoute from './ProtectedRoute'
+import {Meteor} from 'meteor/meteor'
 
 const browserHistory = createBrowserHistory()
 
-const Routes = () => {
+const Routes = ({...props}) => {
+  // const history = useHistory()
+  // const refresh = () => {
+  //   history.push('/notes')
+  // }
+  // function requireAuth(nextState, replace) {
+  //   if (!userExists()) {
+  //     replace({
+  //       pathname: '/welcome',
+  //       state: {nextPathname: nextState.location.pathname},
+  //     })
+  //   }
+  // }
+
   return (
     <Router history={browserHistory}>
       <Switch>
         {/* TODO: delete notes page */}
-        {Meteor.loggingIn() === true ? (
-          browserHistory.push('/notes')
-        ) : (
-          <Route exact path="/welcome" component={Welcome} />
-        )}
+        {/* {Meteor.loggingIn() === true ? (
+          {refresh}
+        ) : ( */}
+        <Route exact path="/welcome" component={Welcome}>
+          {Meteor.loggingIn() === true ? (
+            <Redirect to="/notes" />
+          ) : null}
+        </Route>
+        {/* )} */}
         <ProtectedRoute exact path="/notes" component={Notes} />
         <ProtectedRoute exact path="/" component={Home} />
         <ProtectedRoute
