7dc1a716fc148ce26fea110abdf3391dfe053e3e
EdinK1 <edinkaymakchi@gmail.com>
2019-12-18 18:47:09 -0800

    feat: add updateEmail and username feature

---
+++ imports/ui/components/EditableProfileInfo.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import {Meteor} from 'meteor/meteor'
+import {Input, Icon, Button} from 'antd'
+
+const EditableProfileInfo = ({value, onSave, type = 'text'}) => {
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [input, setInput] = useState(value)
+  const toggleUpdate = () => setIsUpdating(s => !s)
+
+  const onUpdate = e => {
+    if (!input.length) return
+
+    Meteor.call(onSave, input)
+    toggleUpdate()
+    e.preventDefault()
+  }
+
+  return isUpdating ? (
+    <form onSubmit={onUpdate}>
+      <Input
+        type={type}
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      />
+      <Button htmlType="submit">
+        <Icon type="save" />
+      </Button>
+      <Button onClick={toggleUpdate}>
+        <Icon type="close" />
+      </Button>
+    </form>
+  ) : (
+    <>
+      {value}
+      <Button onClick={toggleUpdate}>
+        <Icon type="edit" />
+      </Button>
+    </>
+  )
+}
+
+export default EditableProfileInfo
