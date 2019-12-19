901b392713b11ea06351e684fc12d181d6d60d91
EdinK1 <edinkaymakchi@gmail.com>
2019-12-18 23:08:29 -0800

    feat: add change password to profile page

--- imports/ui/pages/Profile.jsx
+++ imports/ui/pages/Profile.jsx
@@ -3,22 +3,34 @@ import withUser from '../hocs/withUser'
 import {Descriptions} from 'antd'
 import Gravatar from 'react-gravatar'
 import {Layout} from '../components'
-import ProfileUpdate from '../components/ProfileUpdate'
+import EditableProfileInfo from '../components/EditableProfileInfo'
+import ChangePasswordForm from '../components/ChangePasswordForm'
 
 const Profile = ({user, username, email}) => {
   return user ? (
-    <Layout>
-      <Descriptions title="User Info">
-        <Descriptions.Item label="Avatar">
-          <Gravatar default="monsterid" email={email} />
-        </Descriptions.Item>
-        <Descriptions.Item label="User Name">
-          {username}
-        </Descriptions.Item>
-        <Descriptions.Item label="Email">{email}</Descriptions.Item>
-      </Descriptions>
-      <ProfileUpdate />
-    </Layout>
+    console.log(user) || (
+      <Layout>
+        <Descriptions title="User Info">
+          <Descriptions.Item label="Avatar">
+            <Gravatar default="monsterid" email={email} />
+          </Descriptions.Item>
+          <Descriptions.Item label="User name">
+            <EditableProfileInfo
+              onSave={'users.updateUserName'}
+              value={username}
+            />
+          </Descriptions.Item>
+          <Descriptions.Item label="Email">
+            <EditableProfileInfo
+              onSave={'users.updateEmail'}
+              type="email"
+              value={email}
+            />
+          </Descriptions.Item>
+        </Descriptions>
+        <ChangePasswordForm />
+      </Layout>
+    )
   ) : (
     <p>loading...</p>
   )
