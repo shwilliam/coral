f472e8274a831543d9bdcea19e16e7fa315f94da
Roberto H <robhhr@outlook.com>
2019-12-21 17:22:10 -0800

    new note button functionality

--- imports/ui/components/SideMenu.jsx
+++ imports/ui/components/SideMenu.jsx
@@ -21,7 +21,11 @@ const SideMenu = ({notes, sharedNotes, favoriteNotes, ...props}) => {
       {...props}
     >
       <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-        <Menu.Item key="new-note" title="new-note">
+        <Menu.Item
+          key="new-note"
+          title="new-note"
+          onClick={() => history.push('/')}
+        >
           <span>
             <Icon type="plus-circle" theme="outlined" />
             <span>New Note</span>
