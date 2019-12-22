770b87a8b57bef47ca8855d1d3439a0bb9e7c178
EdinK1 <edinkaymakchi@gmail.com>
2019-12-21 17:14:03 -0800

    feat: add styles to profile page

--- imports/ui/pages/Profile/Profile.styles.js
+++ imports/ui/pages/Profile/Profile.styles.js
@@ -1,17 +1,33 @@
 const card = {
-  width: '100%',
+  marginBottom: '24px',
+  width: '80%',
+  padding: '24px',
+  margin: '2rem',
   textAlign: 'center',
 }
 
-const info = {
+const dashed = {
+  border: 'dashed #e8e8e8',
+  borderTopWidth: 'medium',
+  borderRightWidth: 'medium',
+  borderBottomWidth: 'medium',
+  borderLeftWidth: 'medium',
+  borderWidth: '1px 0 0',
+  margin: '1.5rem 0',
   padding: '1rem 0',
+  textAlign: 'left',
+}
+
+const info = {
+  padding: '0.75rem 0',
+  textAlign: 'left',
 }
 
 const gravatar = {
   borderRadius: '50%',
-  width: '60px',
-  height: '60px',
-  marginBottom: '1rem',
+  width: '104px',
+  height: '104px',
+  marginBottom: '20px',
 }
 
-export {card, info, gravatar}
+export {card, dashed, info, gravatar}
