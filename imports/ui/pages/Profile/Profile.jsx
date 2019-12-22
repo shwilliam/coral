becf57417744db49d38ab9ecf04b507d2ba30414
EdinK1 <edinkaymakchi@gmail.com>
2019-12-21 23:25:47 -0800

    feat: add notes and shared notes tabs in profile page

--- imports/ui/pages/Profile/Profile.jsx
+++ imports/ui/pages/Profile/Profile.jsx
@@ -2,13 +2,15 @@ import React, {useState} from 'react'
 import withUser from '../../hocs/withUser'
 import {Meteor} from 'meteor/meteor'
 import {withUsers, withNotes} from '../../hocs'
-import {Skeleton, Icon, List, Card, Typography} from 'antd'
+import {Skeleton, Icon, List, Card, Typography, Tabs} from 'antd'
 const {Text} = Typography
+const {TabPane} = Tabs
 import Gravatar from 'react-gravatar'
 import {Layout} from '../../components'
 import EditableProfileInfo from '../../components/EditableProfileInfo'
 import ChangePasswordForm from '../../components/ChangePasswordForm'
 import {card, info, gravatar, dashed} from './Profile.styles'
+import { Notes } from '/imports/api/notes';
 
 const Profile = ({
   user,
@@ -44,32 +46,35 @@ const Profile = ({
           />
           <ChangePasswordForm />
           <section style={dashed}>
-            <Text strong>Your collaborators</Text>
+            <Text strong>
+              Your collaborators <Icon type="team" />
+            </Text>
             {users
               .filter(({_id}) => !(author === _id))
-              .map(({_id, username}) => (
-                <List.Item key={_id}>
-                  {username === profileUsername ? null : (
-                    <>
-                      <Icon
-                        style={{paddingRight: '0.5rem'}}
-                        type="team"
-                      />
-                      {username}
-                    </>
-                  )}
-                </List.Item>
-              ))}
+              .map(({_id, username}) =>
+                username === profileUsername ? null : (
+                  <List.Item key={_id}>{username}</List.Item>
+                ),
+              )}
           </section>
           <section style={dashed}>
-            <Text strong>Your favorite notes</Text>
+            <Text strong>
+              Your favorite notes <Icon type="star" />
+            </Text>
             {favoriteNotes.map(({_id, title}) => (
-              <List.Item key={_id}>
-                <Icon style={{paddingRight: '0.5rem'}} type="star" />
-                {title}
-              </List.Item>
+              <List.Item key={_id}>{title}</List.Item>
             ))}
           </section>
+        </Card>
+        <Card style={card}>
+          <Tabs defaultActiveKey="1" onChange={callback}>
+            <TabPane tab="Your notes" key="1">
+              Your Notes
+            </TabPane>
+            <TabPane tab="Shared notes" key="2">
+              Shared Notes
+            </TabPane>
+          </Tabs>
         </Card>
       </Layout>
     )
