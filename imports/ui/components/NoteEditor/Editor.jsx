6b801e70e966b7ad45a54061f46644db94cb96e4
Roberto H <robhhr@outlook.com>
2019-12-23 21:57:41 -0800

    change editor position

--- imports/ui/components/NoteEditor/Editor.jsx
+++ imports/ui/components/NoteEditor/Editor.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import Editor from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 
+const styledEditor = {
+  width: '70%',
+  margin: '0 auto',
+}
+
 const DraftEditor = ({value = '', onChange, ...props}) => (
-  <Editor value={value} onChange={onChange} theme="snow" {...props} />
+  <Editor
+    style={styledEditor}
+    value={value}
+    onChange={onChange}
+    theme="snow"
+    {...props}
+  />
 )
 
 export default DraftEditor
