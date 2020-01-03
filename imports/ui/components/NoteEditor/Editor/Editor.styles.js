8f59b86a382f2fb109da76bdd82f7e277e77da14
Roberto H <robhhr@outlook.com>
2020-01-02 20:42:14 -0800

    refactor editor styles

---
+++ imports/ui/components/NoteEditor/Editor/Editor.styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'aphrodite'
+
+const styles = StyleSheet.create({
+  fullEditor: {
+    width: '90%',
+    margin: '0 auto',
+    border: 0,
+    fontSize: '5px',
+  },
+  mobileEditor: {
+    width: '70%',
+    margin: '0 auto',
+    border: 0,
+  },
+})
+
+export default styles
