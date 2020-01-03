7913e2fedadaccddb5ad005bb2b0489ef365d279
EdinK1 <edinkaymakchi@gmail.com>
2020-01-02 20:26:16 -0800

    add auth page as a route

--- imports/ui/router/Router.jsx
+++ imports/ui/router/Router.jsx
@@ -5,6 +5,7 @@ import Home from '../pages/Home'
 import FourOhFour from '../pages/FourOhFour'
 import Note from '../pages/Note'
 import Profile from '../pages/Profile/Profile'
+import Auth from '../pages/Auth'
 import Welcome from '../pages/Welcome'
 import ProtectedRoute from './ProtectedRoute'
 
@@ -15,6 +16,7 @@ const Routes = () => (
     <Switch>
       <ProtectedRoute exact path="/" component={Home} />
       <Route exact path="/welcome" component={Welcome} />
+      <Route exact path="/get-started" component={Auth} />
       <ProtectedRoute exact path="/:username/:id" component={Note} />
       <ProtectedRoute exact path="/profile" component={Profile} />
       <ProtectedRoute component={FourOhFour} />
