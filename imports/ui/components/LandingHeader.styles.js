c242086883b6a11dd2b179a0c38b6f46df64ab22
EdinK1 <edinkaymakchi@gmail.com>
2020-01-02 19:31:41 -0800

    add styles for landing header

---
+++ imports/ui/components/LandingHeader.styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'aphrodite'
+
+const styles = StyleSheet.create({
+  navBar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    listStyleType: 'none',
+    padding: '1rem',
+  },
+})
+
+export default styles
