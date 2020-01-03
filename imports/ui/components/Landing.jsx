6162af00f86de1e523b0bd4e5537de4c72486204
EdinK1 <edinkaymakchi@gmail.com>
2020-01-02 19:31:12 -0800

    add landing component

---
+++ imports/ui/components/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => (
+  <main>
+    <h1>Welcome to coral!</h1>
+    <p>The best note taking app out there!</p>
+    <img src="https://4vector.com/i/free-vector-coral-clip-art_120450_coral-clip-art/Coral_clip_art_hight.png" />
+  </main>
+)
+
+export default Landing
