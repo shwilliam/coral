a34a367a99c7e72ac29fdcfb1280db56b8a48e4b
EdinK1 <edinkaymakchi@gmail.com>
2020-01-03 02:32:55 -0800

    add landing header and features to page

--- imports/ui/pages/Welcome.jsx
+++ imports/ui/pages/Welcome.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import LandingHeader from '../components/LandingHeader'
 import Landing from '../components/Landing'
+import LandingFeatures from '../components/LandingFeatures'
 
 const Welcome = () => {
   return (
     <>
+      <LandingHeader />
       <Landing />
+      <LandingFeatures />
     </>
   )
 }
