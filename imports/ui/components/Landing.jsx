262b7e7e6a83f313a2550843630d21927a4ce065
EdinK1 <edinkaymakchi@gmail.com>
2020-01-03 02:30:03 -0800

    remove landing header compoennt from the file

--- imports/ui/components/Landing.jsx
+++ imports/ui/components/Landing.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import LandingHeader from './LandingHeader'
 import {css} from 'aphrodite'
 import styles from './Landing.styles'
 
 const Landing = () => (
-  <>
-    <LandingHeader />
-    <main className={css(styles.mainContent)}>
-      <h1 className={css(styles.heading)}>Welcome to coral!</h1>
-      <p>The best note taking app out there!</p>
-    </main>
-  </>
+  <main className={css(styles.mainContent)}>
+    <h1 className={css(styles.heading)}>Welcome to coral!</h1>
+    <p>The best note taking app out there!</p>
+  </main>
 )
 
 export default Landing
