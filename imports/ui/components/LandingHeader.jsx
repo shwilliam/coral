c4f571a3f871ee4b91f6a73ccfbbc64f304d16ad
EdinK1 <edinkaymakchi@gmail.com>
2020-01-02 20:24:55 -0800

    add onClick to get started button

--- imports/ui/components/LandingHeader.jsx
+++ imports/ui/components/LandingHeader.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 import {Button} from 'antd'
 import {css} from 'aphrodite'
 import styles from './Landing.styles'
+import {useHistory} from 'react-router'
 
-const LandingHeader = () => (
-  <header>
-    <nav>
-      <ul className={css(styles.navBar)}>
-        <li>coral</li>
-        <li>
-          <Button type="primary">Get Started</Button>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
+const LandingHeader = () => {
+  const history = useHistory()
+
+  return (
+    <header>
+      <nav>
+        <ul className={css(styles.navBar)}>
+          <li>coral</li>
+          <li>
+            <Button onClick={() => history.push('/get-started')} type="primary">
+              Get Started
+            </Button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
 export default LandingHeader
