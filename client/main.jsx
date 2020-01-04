b829bbc366ff63ad561bdb7d36097dfe12420615
William L <w-lindvall@outlook.com>
2020-01-04 15:47:15 -0800

    set up theme context

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Meteor} from 'meteor/meteor'
 import {render} from 'react-dom'
-import Routes from '../imports/ui/router/Router'
+import App from '../imports/ui/App'
 
 // ensure Meteor methods are available
 import * as _ from '../imports/api/users'
@@ -10,5 +10,5 @@ import * as __ from '../imports/api/notes'
 import 'antd/dist/antd.css' // TODO: use modularized styles
 
 Meteor.startup(() => {
-  render(<Routes />, document.getElementById('react-target'))
+  render(<App />, document.getElementById('react-target'))
 })
