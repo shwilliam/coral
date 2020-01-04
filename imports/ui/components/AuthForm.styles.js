e24eac05ed8c199554f7ac2f988642e14b2e5c9a
Roberto H <robhhr@outlook.com>
2020-01-04 13:48:39 -0800

    auth form styles w/ aphrodite

---
+++ imports/ui/components/AuthForm.styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'aphrodite'
+
+const styles = StyleSheet.create({
+  form: {
+    width: '200px',
+    margin: '0 auto',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+})
+
+export default styles
