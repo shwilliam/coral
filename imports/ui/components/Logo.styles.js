41888af8253e9dcc7e9f79684011d0e4a0925a58
Roberto H <robhhr@outlook.com>
2020-01-05 13:24:55 -0800

    logo component animation

--- imports/ui/components/Logo.styles.js
+++ imports/ui/components/Logo.styles.js
@@ -1,8 +1,33 @@
 import {StyleSheet} from 'aphrodite'
 
+const typing = {
+  from: {
+    width: 0,
+  },
+  to: {
+    width: '100%',
+  },
+}
+
+const blinkCursor = {
+  from: {borderColor: 'transparent'},
+  to: {borderColor: 'transparent'},
+  '100%': {borderColor: '#1890ff'},
+}
+
 const styles = StyleSheet.create({
   logo: {
-    textAlign: 'center',
+    fontSize: '38px',
+    maxWidth: '120px',
+    overflow: 'hidden',
+    borderRight: '.15em solid #1890ff',
+    whiteSpace: 'nowrap',
+    margin: '20px auto',
+    letterSpacing: '.15em',
+    transition: 'ease-in-out',
+    animationName: [typing, blinkCursor],
+    animationDuration: '2.5s',
+    animationIterationCount: 'infinite',
   },
 })
 
