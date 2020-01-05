adb936b632318ca37bcb2f763b06f6323265ba14
EdinK1 <edinkaymakchi@gmail.com>
2020-01-05 12:07:18 -0800

    add styles to component

--- imports/ui/components/Landing/Showcase.jsx
+++ imports/ui/components/Landing/Showcase.jsx
@@ -5,7 +5,9 @@ import styles from './Landing.styles'
 const Showcase = () => (
   <main className={css(styles.mainContent)}>
     <h1 className={css(styles.heading)}>Welcome to coral!</h1>
-    <p>The best note taking app out there!</p>
+    <p className={css(styles.paragraph)}>
+      The best note taking app out there!
+    </p>
   </main>
 )
 
