d09e14cba7da37ae3b46dff8d03c918926881afc
EdinK1 <edinkaymakchi@gmail.com>
2020-01-04 23:45:04 -0800

    add all components to welcome page and use back to top and divider component from ant desing

--- imports/ui/pages/Welcome.jsx
+++ imports/ui/pages/Welcome.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import {Header, Showcase, Features} from '../components/Landing/'
+import {
+  Header,
+  Showcase,
+  Features,
+  Developers,
+  Footer,
+} from '../components/Landing/'
+import {BackTop} from 'antd'
+import {Divider} from 'antd'
 
 const Welcome = () => {
   return (
     <>
+      <BackTop />
       <Header />
       <Showcase />
+      <Divider />
       <Features />
+      <Divider />
+      <Developers />
+      <Divider />
+      <Footer />
     </>
   )
 }
