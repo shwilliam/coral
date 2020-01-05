1f4c67bb96b267a0d1a89b4855c10e493fa62de9
EdinK1 <edinkaymakchi@gmail.com>
2020-01-05 12:02:50 -0800

    wrap component around a section tag and add its own styles

--- imports/ui/components/Landing/Developers.jsx
+++ imports/ui/components/Landing/Developers.jsx
@@ -4,33 +4,38 @@ import styles from './Landing.styles'
 import Developer from './Developer'
 
 const Developers = () => (
-  <section className={css([styles.container, styles.devContainer])}>
+  <section className={css(styles.container)}>
     <h1>The masterminds behind it</h1>
     <p>Meet the talented team that built coral.</p>
-    <Developer
-      imgStyle={css([styles.landingImgs, styles.devImgs])}
-      imgSrc="/shwilliam.png"
-      name="William Lindvall"
-      linkStyles={css(styles.devLinks)}
-      githubLink="https://github.com/shwilliam"
-      websiteLink="https://shwilliam.com"
-    />
-    <Developer
-      imgStyle={css([styles.landingImgs, styles.devImgs])}
-      imgSrc="/edin.png"
-      name="Edin Kaymakqi"
-      linkStyles={css(styles.devLinks)}
-      githubLink="https://github.com/EdinK1"
-      websiteLink="https://edin.rocks"
-    />
-    <Developer
-      imgStyle={css([styles.landingImgs, styles.devImgs])}
-      imgSrc="/robhhr.png"
-      name="Roberto Hernandez"
-      linkStyles={css(styles.devLinks)}
-      githubLink="https://github.com/robhhr"
-      websiteLink="https://robhhr.com"
-    />
+    <section className={css(styles.features)}>
+      <Developer
+        developerStyles={css(styles.feature)}
+        imgStyle={css([styles.landingImgs, styles.devImgs])}
+        imgSrc="/shwilliam.png"
+        name="William Lindvall"
+        linkStyles={css(styles.devLinks)}
+        githubLink="https://github.com/shwilliam"
+        websiteLink="https://shwilliam.com"
+      />
+      <Developer
+        developerStyles={css(styles.feature)}
+        imgStyle={css([styles.landingImgs, styles.devImgs])}
+        imgSrc="/edin.png"
+        name="Edin Kaymakqi"
+        linkStyles={css(styles.devLinks)}
+        githubLink="https://github.com/EdinK1"
+        websiteLink="https://edin.rocks"
+      />
+      <Developer
+        developerStyles={css(styles.feature)}
+        imgStyle={css([styles.landingImgs, styles.devImgs])}
+        imgSrc="/robhhr.png"
+        name="Roberto Hernandez"
+        linkStyles={css(styles.devLinks)}
+        githubLink="https://github.com/robhhr"
+        websiteLink="https://robhhr.com"
+      />
+    </section>
   </section>
 )
 
