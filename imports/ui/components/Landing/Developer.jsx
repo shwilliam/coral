23f9fbd571694fda4d1419bb00407d35a5a0c0c0
EdinK1 <edinkaymakchi@gmail.com>
2020-01-05 12:02:25 -0800

    wrap component around a section tag

--- imports/ui/components/Landing/Developer.jsx
+++ imports/ui/components/Landing/Developer.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import {Icon} from 'antd'
 
 const Developer = ({
+  developerStyles,
   imgStyle,
   imgSrc,
   name,
   linkStyles,
   githubLink,
   websiteLink,
+  ...props
 }) => (
-  <>
+  <section className={developerStyles} {...props}>
     <img className={imgStyle} src={imgSrc} alt={name} />
     <h2>{name}</h2>
     <a className={linkStyles} href={githubLink}>
@@ -18,7 +20,7 @@ const Developer = ({
     <a className={linkStyles} href={websiteLink}>
       <Icon type="link" />
     </a>
-  </>
+  </section>
 )
 
 export default Developer
