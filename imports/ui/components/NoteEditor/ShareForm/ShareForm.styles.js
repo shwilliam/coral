907e3d5a4a14c69f39103b7e3106d631d1365569
Roberto H <robhhr@outlook.com>
2020-01-05 00:10:11 -0800

    refactor shareform styles

--- imports/ui/components/NoteEditor/ShareForm/ShareForm.styles.js
+++ imports/ui/components/NoteEditor/ShareForm/ShareForm.styles.js
@@ -2,6 +2,9 @@ import {StyleSheet} from 'aphrodite'
 
 const styles = StyleSheet.create({
   form: {
+    '@media (min-width: 480px)': {
+      width: '90%',
+    },
     width: '70%',
     margin: '0 auto',
   },
