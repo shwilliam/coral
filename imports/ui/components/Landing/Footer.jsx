e1da30c5b237c1dd3772e480ba75efeda3b95ad8
EdinK1 <edinkaymakchi@gmail.com>
2020-01-04 23:43:22 -0800

    create footer component

---
+++ imports/ui/components/Landing/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {css} from 'aphrodite'
+import styles from './Landing.styles'
+
+const Footer = () => (
+  <footer className={css(styles.footer)}>
+    <p>© 2019 Coral. All rights reserved</p>
+  </footer>
+)
+
+export default Footer
