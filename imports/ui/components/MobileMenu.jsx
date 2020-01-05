c94617a7bda80e09293fdacdf2b47dc8bdbeba24
EdinK1 <edinkaymakchi@gmail.com>
2020-01-05 14:40:27 -0800

    fix: update mobile menu

--- imports/ui/components/MobileMenu.jsx
+++ imports/ui/components/MobileMenu.jsx
@@ -1,96 +1,104 @@
 import React from 'react'
-const {SubMenu} = Menu
 import {Menu, Icon} from 'antd'
+import {useHistory} from 'react-router'
 import {withNotes} from '../hocs'
-import {useHistory, useParams} from 'react-router'
 import {Meteor} from 'meteor/meteor'
+import {useTheme} from '../hooks'
+import {activeNote} from '../../api/notes'
+import {
+  sideMenuLightStyles,
+  sideMenuDarkStyles,
+  sideMenuContentLightStyles,
+  sideMenuContentDarkStyles,
+} from './SideMenu/SideMenu.styles'
 
 const MobileMenu = ({
-  favoriteNotes,
   notes,
   sharedNotes,
+  favoriteNotes,
   ...props
 }) => {
+  const [theme] = useTheme()
   const history = useHistory()
   const logout = () =>
     Meteor.logout(() => location.replace('/welcome'))
   return (
-    <Menu
-      theme="dark"
-      style={{textAlign: 'center'}}
-      mode="horizontal"
-    >
-      <Menu.SubMenu
-        key="side-menu-favorites"
-        title={
-          <span>
-            <Icon type="star" />
-          </span>
+    <nav style={{textAlign: 'center'}}>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        style={
+          theme === 'light'
+            ? sideMenuContentLightStyles
+            : sideMenuContentDarkStyles
         }
       >
-        {favoriteNotes.map(({_id, title}) => (
-          <Menu.Item
-            key={_id}
-            onClick={() => history.push(`/note/${_id}`)}
-          >
-            <span>{title}</span>
-          </Menu.Item>
-        ))}
-      </Menu.SubMenu>
-      <SubMenu
-        title={
-          <span className="submenu-title-wrapper">
-            <Icon type="edit" />
-          </span>
-        }
-      >
-        <Menu.ItemGroup title="My Notes">
+        <Menu.Item
+          key="new-note"
+          title="new-note"
+          onClick={() => history.push('/')}
+        >
+          <Icon type="plus-circle" theme="outlined" />
+        </Menu.Item>
+        <Menu.SubMenu
+          key="side-menu-favorites"
+          title={<Icon type="star" />}
+        >
+          {favoriteNotes.map(({_id, title}) => (
+            <Menu.Item
+              key={_id}
+              onClick={() => history.push(`/note/${_id}`)}
+            >
+              <span>{title}</span>
+            </Menu.Item>
+          ))}
+        </Menu.SubMenu>
+        <Menu.SubMenu
+          key="side-menu-notes"
+          title={<Icon type="edit" />}
+        >
           {notes.map(({_id, title}) => (
-            <Menu.Item key={_id}>
-              <span>
-                <a href={`/note/${_id}`}>{title}</a>
-              </span>
+            <Menu.Item
+              key={_id}
+              onClick={() => {
+                activeNote.set(`/note/${_id}`)
+                history.push(`/note/${_id}`)
+              }}
+            >
+              <span>{title}</span>
             </Menu.Item>
           ))}
-        </Menu.ItemGroup>
-      </SubMenu>
-      <SubMenu
-        title={
-          <span className="submenu-title-wrapper">
-            <Icon type="team" />
-          </span>
-        }
-      >
-        <Menu.ItemGroup title="Shared Notes">
+        </Menu.SubMenu>
+        <Menu.SubMenu
+          key="side-menu-shared"
+          title={<Icon type="team" />}
+        >
           {sharedNotes.map(({_id, title}) => (
-            <Menu.Item key={_id}>
-              <span>
-                <a href={`/note/${_id}`}>{title}</a>
-              </span>
+            <Menu.Item
+              key={_id}
+              onClick={() => history.push(`/note/${_id}`)}
+            >
+              <span>{title}</span>
             </Menu.Item>
           ))}
-        </Menu.ItemGroup>
-      </SubMenu>
-      <SubMenu
-        title={
-          <span className="submenu-title-wrapper">
-            <Icon type="setting" />
-          </span>
-        }
-      >
-        <Menu.ItemGroup title="Settings">
-          <Menu.Item key="logout" onClick={logout}>
-            <Icon type="logout" /> Logout
-          </Menu.Item>
+        </Menu.SubMenu>
+        <Menu.SubMenu
+          key="side-menu-settings"
+          title={<Icon type="setting" />}
+        >
           <Menu.Item
             key="profile"
             onClick={() => history.push('/profile')}
           >
             <Icon type="user" /> Profile
           </Menu.Item>
-        </Menu.ItemGroup>
-      </SubMenu>
-    </Menu>
+          <Menu.Item key="logout" onClick={logout}>
+            <Icon type="logout" /> Logout
+          </Menu.Item>
+        </Menu.SubMenu>
+      </Menu>
+    </nav>
   )
 }
 
