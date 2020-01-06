e3d49599c58638d1869478d12aed027ad3b9c267
EdinK1 <edinkaymakchi@gmail.com>
2020-01-05 23:41:50 -0800

    feat: add first time tutorial

--- imports/ui/components/Splash/Splash.jsx
+++ imports/ui/components/Splash/Splash.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Joyride from 'react-joyride'
 import {useTheme} from '../../hooks'
-import {Typography} from 'antd'
+import {Typography, Button} from 'antd'
 import {
   splashTextStyles,
   splashContainerStyles,
@@ -10,6 +11,30 @@ import {
 
 const Splash = ({children, ...props}) => {
   const [theme] = useTheme()
+  const [run, setRun] = useState(true)
+  const [steps, setSteps] = useState([
+    {
+      target: '.anticon.anticon-plus-circle',
+      content: 'Use this to create a new note!',
+    },
+    {
+      target: '.anticon.anticon-star',
+      content: 'This is where all your favorite notes will appear.',
+    },
+    {
+      target: '.anticon.anticon-edit',
+      content: 'This is where all your notes will appear.',
+    },
+    {
+      target: '.anticon.anticon-team',
+      content: 'This is where all your shared notes will appear.',
+    },
+    {
+      target: '.anticon.anticon-setting',
+      content:
+        'This is where you can access your profile info and logout.',
+    },
+  ])
 
   return (
     <div style={splashContainerStyles} {...props}>
@@ -20,6 +45,17 @@ const Splash = ({children, ...props}) => {
         <Typography.Paragraph style={splashTextStyles[theme]}>
           {children}
         </Typography.Paragraph>
+        <Joyride
+          steps={steps}
+          continuous={true}
+          showProgress={true}
+          showSkipButton={true}
+          styles={{
+            options: {
+              zIndex: 10000,
+            },
+          }}
+        />
       </div>
       <div style={splashImageStyles[theme]} />
     </div>
