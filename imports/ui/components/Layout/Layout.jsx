d47ec43ef7758ca0f69e29a72f127fdd51653660
William L <w-lindvall@outlook.com>
2020-01-05 18:09:47 -0800

    style solarized layout container

--- imports/ui/components/Layout/Layout.jsx
+++ imports/ui/components/Layout/Layout.jsx
@@ -5,8 +5,8 @@ import SideMenu from '../SideMenu'
 import MobileMenu from '../MobileMenu'
 const {Content} = AntLayout
 import {
-  contentContainerLightStyles,
-  contentContainerDarkStyles,
+  layoutContainerStyles,
+  contentContainerStyles,
 } from './Layout.styles'
 
 const isMobile = window.innerWidth < 480
@@ -15,15 +15,14 @@ const Layout = ({children, ...props}) => {
   const [theme] = useTheme()
 
   return (
-    <AntLayout {...props}>
+    <AntLayout
+      style={
+        theme === 'solarized' ? layoutContainerStyles.fullHeight : {}
+      }
+      {...props}
+    >
       {isMobile ? <MobileMenu /> : <SideMenu />}
-      <AntLayout
-        style={
-          theme === 'light'
-            ? contentContainerLightStyles
-            : contentContainerDarkStyles
-        }
-      >
+      <AntLayout style={contentContainerStyles[theme]}>
         <Content>{children}</Content>
       </AntLayout>
     </AntLayout>
