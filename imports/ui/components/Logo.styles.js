11d7f890adc1060f89f27faa76d3d43a59b9501e
Roberto H <robhhr@outlook.com>
2020-01-05 18:00:29 -0800

    remove extraneous comments

--- imports/ui/components/Logo.styles.js
+++ imports/ui/components/Logo.styles.js
@@ -18,13 +18,6 @@ const blink = {
 }
 
 const styles = StyleSheet.create({
-  // logoContainer: {
-  //   // display: 'flex',
-  //   // alignItems: 'center',
-  //   // height: '50px',
-  //   // width: '130px',
-  //   // // margin: '25px auto',
-  // },
   logo: {
     fontFamily: 'Quicksand',
     fontSize: '38px',
