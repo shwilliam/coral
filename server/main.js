9fde244f76cf2bdb8af6f3eca705acabf9d4afb2
William L <w-lindvall@outlook.com>
2020-01-05 22:31:39 -0800

    hotfix: remove dummy notes insert on startup

--- server/main.js
+++ server/main.js
@@ -1,21 +1,7 @@
 import {Meteor} from 'meteor/meteor'
-import Notes from '/imports/api/notes'
 
 // ensure Meteor methods are available
 import * as _ from '../imports/api/users'
 import * as __ from '../imports/api/notes'
 
-function insertNote(title, content) {
-  Notes.insert({title, content, createdAt: new Date()})
-}
-
-Meteor.startup(() => {
-  // if no notes, add dummy data
-  if (Notes.find().count() === 0) {
-    insertNote('My first note', 'bla bla')
-
-    insertNote('My second note', 'bla bla')
-
-    insertNote('My third note', 'bla bla')
-  }
-})
+Meteor.startup(() => {})
