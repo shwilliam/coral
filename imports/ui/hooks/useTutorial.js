111afede4646168df0ff28cf722d0e35df7d5afb
William L <w-lindvall@outlook.com>
2020-01-06 13:06:37 -0800

    update tutorial text

--- imports/ui/hooks/useTutorial.js
+++ imports/ui/hooks/useTutorial.js
@@ -3,25 +3,38 @@ import {useState} from 'react'
 const useTutorial = () => {
   const [steps] = useState([
     {
-      target: '.anticon.anticon-plus-circle',
-      content: 'Use this to create a new note!',
+      target: 'h1.ant-typography',
+      content:
+        'Welcome to coral! This short walkthrough will highlight some of the main features and help you navigate the app.',
     },
     {
-      target: '.anticon.anticon-star',
-      content: 'This is where all your favorite notes will appear.',
+      target: '.anticon.anticon-plus-circle',
+      content:
+        "The 'New Note' button allows you to create and open a new note.",
     },
     {
       target: '.anticon.anticon-edit',
-      content: 'This is where all your notes will appear.',
+      content: 'When created, all your notes can be found here.',
+    },
+    {
+      target: '.anticon.anticon-star',
+      content:
+        "Notes marked as 'favorites' can be easily accessed here.",
     },
     {
       target: '.anticon.anticon-team',
-      content: 'This is where all your shared notes will appear.',
+      content:
+        'If another user adds you as a collaborator, the shared note will be available to you here.',
     },
     {
       target: '.anticon.anticon-setting',
       content:
-        'This is where you can access your profile info and logout.',
+        "Under the settings tab you will find a link to the 'Profile' page where you can access, update or change your profile information and change the app's theme.",
+    },
+    {
+      target: 'h1.ant-typography',
+      content:
+        "That's about it! We hope coral helps you organize your ideas and get things done.",
     },
   ])
 
